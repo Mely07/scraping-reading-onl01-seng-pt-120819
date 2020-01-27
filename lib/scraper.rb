dd06350e09e8a2c9bd305aa0f06894204a63231c
Damely Tineo <damely.tineo07@gmail.com>
2020-01-27 01:39:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,7 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html) #Nokogiri::HTML(html) -- method used to take the string of HTML returned by open-uri's open method method and convert it into a NodeSet(a bunch of nested "nodes")
-#Nested nodes refers to any tree of elements in which parent elements branch off to contain children elements. In fact, we've seen similarly nested structures before when we dealt with nested data structures like hashes. By creating a nested structure, Nokogiri allows us to do things like iterate over a collection of elements from the HTML document and use brackets,[], and dot notation to access elements within the nested structure.
+doc = Nokogiri::HTML(html) 
 
 puts doc.css(".headline-26OIBN").text
 
